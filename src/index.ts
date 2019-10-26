a5a1a97800eabae6a7711d0646864f04f84fb907
rahim <609787932@qq.com>
2019-10-26 17:38:11 +0800

    fix: d.ts文件

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import Taro  from '@tarojs/taro'
 import ListView from './components/list-view/index';
 
-Taro.initPxTransform({ designWidth: 750 });
+Taro.initPxTransform({ designWidth: 750,
+  deviceRatio: {
+    "640": 1.17,
+    "750": 1,
+    "828": 0.905
+  }});
 
 export { default as Skeleton } from './components/skeleton/index';
 export { default as ListView } from './components/list-view/index';
